3a4a73e019f01710f2f4ba26bc855cf220845458
fauu <fau999@gmail.com>
2017-06-30 15:11:24 +0200

    Make prepend builtin work with string expressions

--- src/main/java/com/github/fauu/flij/builtin/PrependBuiltin.java
+++ src/main/java/com/github/fauu/flij/builtin/PrependBuiltin.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 import com.github.fauu.flij.evaluator.Environment;
 import com.github.fauu.flij.evaluator.ExpressionEvaluator;
+import com.github.fauu.flij.expression.CharacterExpression;
 import com.github.fauu.flij.expression.Expression;
 import com.github.fauu.flij.expression.ListExpression;
+import com.github.fauu.flij.expression.StringExpression;
 
 @RegisteredBuiltin("prepend")
 public class PrependBuiltin extends Builtin {
@@ -22,6 +24,12 @@ public class PrependBuiltin extends Builtin {
 
     Expression element = evaluator.evaluate(arguments.get(0), environment);
     Expression evaluatedListArgument = evaluator.evaluate(arguments.get(1), environment);
+
+    if (evaluatedListArgument instanceof StringExpression) {
+      CharacterExpression character = ensureArgumentType(element, CharacterExpression.class);
+      return new StringExpression(character.getValue() + ((StringExpression) evaluatedListArgument).getValue());
+    }
+    
     ListExpression list = ensureArgumentType(evaluatedListArgument, ListExpression.class);
     
     List<Expression> newElements = new LinkedList<Expression>();
